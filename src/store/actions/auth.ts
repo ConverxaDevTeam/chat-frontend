fb741eecbfcb62cfd06a01c9c7b9fedbcc22854d
Jose Leonardo Agreda Anchaise <joseleonardoagreda@gmail.com>
2024-12-13 21:30:44 -0300

    update script source and clean up route formatting

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -7,6 +7,7 @@ import { jwtDecode } from "jwt-decode";
 import {
   connectWebSocket,
   disconnectWebSocket,
+  joinRoom,
   onWebSocketEvent,
 } from "@services/websocket.service";
 import { newMessageChat } from "./conversations";
