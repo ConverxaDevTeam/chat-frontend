bebf7cbded18113b273b83cce9157c1976aca645
frank <frank@pixeldigita.com>
2025-01-08 16:34:11 -0600

    fix for departments

--- src/components/Table/TableCell.tsx
+++ src/components/Table/TableCell.tsx
@@ -10,8 +10,8 @@ const TableCell: FC<TableCellProps> = ({ width, hidden, children }) => {
   if (hidden) return null;
 
   return (
-    <td className={`${width || ""} px-4`}>
-      <p className="font-poppinsRegular text-[#212121]">{children}</p>
+    <td className={`${width || ""} px-4 font-poppinsRegular text-[#212121]`}>
+      {children}
     </td>
   );
 };
