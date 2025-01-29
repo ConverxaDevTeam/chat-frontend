6167704de572af54ae7726f73412e1a659f14e6d
frank <frank@pixeldigita.com>
2025-01-29 11:58:01 -0600

    arreglox menu funciones

--- src/components/Diagrams/components/DiagramContextMenuV2.tsx
+++ src/components/Diagrams/components/DiagramContextMenuV2.tsx
@@ -21,7 +21,7 @@ export const DiagramContextMenuV2: React.FC<DiagramContextMenuV2Props> = ({
         <button
           key={index}
           onClick={option.onClick}
-          className="flex justify-center items-center rounded-lg hover:bg-sofia-electricOlive/10 transition-colors gap-[10px]"
+          className="flex w-full items-center rounded-lg hover:bg-sofia-electricOlive/10 transition-colors gap-[10px]"
         >
           {option.child}{" "}
           <span className="text-[#001126] font-quicksand text-[14px] font-[500] leading-normal">
