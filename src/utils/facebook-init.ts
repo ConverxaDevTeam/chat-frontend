fc45c8213e16a0c68984de3e1fc978a3a55b4d9f
frank <frank@pixeldigita.com>
2025-06-05 15:57:45 -0600

    chore: update Facebook SDK version to v23.0 and remove debug logs

--- src/utils/facebook-init.ts
+++ src/utils/facebook-init.ts
@@ -70,22 +70,13 @@ export function initFacebookSDK(): Promise<typeof window.FB> {
       }
 
       try {
-        console.log("SDK de Facebook cargado, inicializando...");
-        console.log({
-          appId: import.meta.env.VITE_FB_APP_ID,
-          autoLogAppEvents: true,
-          status: true,
-          cookie: true,
-          xfbml: true,
-          version: "v22.0",
-        });
         window.FB.init({
           appId: import.meta.env.VITE_FB_APP_ID,
           autoLogAppEvents: true,
           status: true,
           cookie: true,
           xfbml: true,
-          version: "v22.0",
+          version: "v23.0",
         });
 
         isFBInitialized = true;
